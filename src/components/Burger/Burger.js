6fe5b3ece1b81d1b6439f57d53fbd9ea92647b3a
codershu <32534720+hanshudream@users.noreply.github.com>
2018-10-15 15:46:17 -0500

    created basic burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,1 +1,21 @@
 import React from 'react';
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    let ingredients = Object.keys(props.ingredients).map(key => {
+        return [...Array(props.ingredients[key])].map((_, index) => {
+            return <BurgerIngredient key={key + index} type={key} />
+        })
+    });
+    // console.log("ingredients", ingredients)
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {ingredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+}
+
+export default burger;
